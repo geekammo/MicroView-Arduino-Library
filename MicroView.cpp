cc1d78384aabe39f1cae3e354ba159cbfc02b114
Scott Allen <scotta_ym@yahoo.ca>
2014-08-04 19:20:18 -0400

    Changed vertical slider direction. Low values at the bottom. High values at the top.

--- MicroView.cpp
+++ MicroView.cpp
@@ -1541,12 +1541,13 @@ void MicroViewSlider::draw() {
 	offsetY=getY();
 
 	if (needFirstDraw) {
-		tickPosition= (((float)(prevValue-getMinValue())/(float)(getMaxValue()-getMinValue()))*totalTicks);
 		if (style==0 || style==1){		//Horizontal
+			tickPosition= (((float)(prevValue-getMinValue())/(float)(getMaxValue()-getMinValue()))*totalTicks);
 			uView.lineH(offsetX+tickPosition,offsetY, 3, WHITE, XOR);
 			uView.pixel(offsetX+1+tickPosition,offsetY+1, WHITE, XOR);
 		}
 		else {					//Vertical
+			tickPosition= (((float)(getMaxValue()-prevValue)/(float)(getMaxValue()-getMinValue()))*totalTicks);
 			uView.lineV(offsetX+7, offsetY+tickPosition, 3, WHITE, XOR);
 			uView.pixel(offsetX+6, offsetY+1+tickPosition, WHITE, XOR);
 		}
@@ -1556,23 +1557,25 @@ void MicroViewSlider::draw() {
 	}
 	else {
 		// Draw previous pointer in XOR mode to erase it
-		tickPosition= (((float)(prevValue-getMinValue())/(float)(getMaxValue()-getMinValue()))*totalTicks);
 		if (style==0 || style==1){		//Horizontal
+			tickPosition= (((float)(prevValue-getMinValue())/(float)(getMaxValue()-getMinValue()))*totalTicks);
 			uView.lineH(offsetX+tickPosition,offsetY, 3, WHITE, XOR);
 			uView.pixel(offsetX+1+tickPosition,offsetY+1, WHITE, XOR);
 		}
 		else {					//Vertical
+			tickPosition= (((float)(getMaxValue()-prevValue)/(float)(getMaxValue()-getMinValue()))*totalTicks);
 			uView.lineV(offsetX+7, offsetY+tickPosition, 3, WHITE, XOR);
 			uView.pixel(offsetX+6, offsetY+1+tickPosition, WHITE, XOR);
 		}
 
 		// Draw current pointer
-		tickPosition= (((float)(getValue()-getMinValue())/(float)(getMaxValue()-getMinValue()))*totalTicks);
 		if (style==0 || style==1){		//Horizontal
+			tickPosition= (((float)(getValue()-getMinValue())/(float)(getMaxValue()-getMinValue()))*totalTicks);
 			uView.lineH(offsetX+tickPosition,offsetY, 3, WHITE, XOR);
 			uView.pixel(offsetX+1+tickPosition,offsetY+1, WHITE, XOR);
 		}
 		else {					//Vertical
+			tickPosition= (((float)(getMaxValue()-getValue())/(float)(getMaxValue()-getMinValue()))*totalTicks);
 			uView.lineV(offsetX+7, offsetY+tickPosition, 3, WHITE, XOR);
 			uView.pixel(offsetX+6, offsetY+1+tickPosition, WHITE, XOR);
 		}
